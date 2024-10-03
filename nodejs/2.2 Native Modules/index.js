da900fb9eaa7d6b05eadcbb16a292dc77f4075da
kishor kumar <kishorkumarbabu5@gmail.com>
2024-10-03 21:46:28 +0530

    js

---
+++ nodejs/2.2 Native Modules/index.js
@@ -0,0 +1,13 @@
+const fs=require("fs")
+/* fs.writeFile("messaage.txt","hello fron node js", (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  }); */
+
+
+
+  fs.readFile("./messaage.txt", (err, data) => {
+      if (err) throw err;
+      console.log(data.toString());  // Convert buffer to string
+  });
+  
---
+++ nodejs/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+/* var generateName = require('sillyname'); */
+/* import generateName from "sillyname"
+var sillyName = generateName();
+console.log('my name is '+sillyName) */
+import superheroes from 'superheroes';
+var sillyName = superheroes.random();
+console.log('my name is '+sillyName)
